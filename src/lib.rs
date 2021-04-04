a21d663439e067ae5b238a9f898f2ed1547dd5ca
Brenden Matthews <brenden@brndn.io>
2021-04-04 13:35:57 -0500

    Update docs.

--- src/lib.rs
+++ src/lib.rs
@@ -2,27 +2,29 @@
 # dryoc: Don't Roll Your Own Crypto™[^1]
 
 dryoc is a pure-Rust implementation of
-[libsodium](https://libsodium.gitbook.io/doc/), intended to be 100%
+[libsodium](https://libsodium.gitbook.io/doc/), designed to be 100%
 compatible with a interchangeable with the libsodium API, and have limited
 dependencies.
+
+Minimum supported Rust version (MSRV): Requires **Rust 1.51** or newer.
 
 ## Features
 
 * 100% pure Rust
 * mostly free of unsafe code[^2]
-* free of corporate or governmental influence
-* automatic safety features such as zeroization of data structures (via [Drop])
-* protected memory support, with the API designed such that it's hard to
+* free of corporate and governmental influence
+* automatic safety features such as zeroization of data structures (via
+  [Drop] and [Zeroize](https://crates.io/crates/zeroize))
+* protected memory support, with an API designed such that it's hard to
   accidentally unprotect your memory
-* it's designed to be especially difficult to use incorrectly (with the
-  Rustaceous API)
+* designed to be especially difficult to use incorrectly[^3]
 * provides full compatibility with libsodium, and implements most of its functions
 * covers common use cases for safe encryption
 * available under the [LGPL-3.0 license](https://www.gnu.org/licenses/lgpl-3.0.en.html)
 
 # APIs
 
-This library includes both a _classic_ API, which is very similar to the
+This library includes both a _Classic_ API, which is very similar to the
 original libsodium API, and _Rustaceous_ API with Rust-specific features.
 Both APIs can be used together interchangeably, according to your
 preferences. The Rustaceous API is a wrapper around the underlying classic
@@ -37,14 +39,16 @@ have to try really hard to do things wrong.
 | Feature                        | Rustaceous API   | Classic API                             | Libsodium Docs                                                                            |
 |--------------------------------|------------------|-----------------------------------------|-------------------------------------------------------------------------------------------|
 | Secret-key authenticated boxes | [dryocsecretbox] | [crypto_secretbox]                      | [Link](https://libsodium.gitbook.io/doc/secret-key_cryptography/secretbox)                |
-| Streaming encryption           | [dryocstream]    | [crypto_secretstream_xchacha20poly1305] | [Link](https://libsodium.gitbook.io/doc/secret-key_cryptography/secretstream)             |
 | Public-key authenticated boxes | [dryocbox]       | [crypto_box]                            | [Link](https://libsodium.gitbook.io/doc/public-key_cryptography/authenticated_encryption) |
-| Protected memory[^3]           | [protected]      | N/A                                     | [Link](https://doc.libsodium.org/memory_management)                                       |
+| Streaming encryption           | [dryocstream]    | [crypto_secretstream_xchacha20poly1305] | [Link](https://libsodium.gitbook.io/doc/secret-key_cryptography/secretstream)             |
+| Protected memory[^4]           | [protected]      | N/A                                     | [Link](https://doc.libsodium.org/memory_management)                                       |
 
 # Using Serde
 
 This crate includes optional [Serde](https://serde.rs/) support which can be
-enabled with the `serde` feature flag.
+enabled with the `serde` feature flag. When enabled, the
+[`Serialize`](serde::ser::Serialize) and [`Deserialize`](serde::de::Deserialize)
+traits are provided for data structures.
 
 # Security notes
 
@@ -54,14 +58,17 @@ With that out of the way, the deterministic nature of cryptography and
 extensive testing used in this crate means it's relatively safe to use,
 provided the underlying algorithms remain safe. Arguably, this crate is
 _incredibly_ safe (as far as cryptography libraries go) thanks to the
-features provided by the Rust language.
+features of the Rustaceous API, and those provided by the Rust language.
 
 [^1]: Not actually trademarked.
 
 [^2]: The protected memory features described in [protected] require custom
 memory allocation and system calls, which are unsafe in Rust.
 
-[^3]: Currently only available on nightly Rust, with the `nightly` feature flag enabled.
+[^3]: The Rustaceous API is designed to protect users of this library from
+making mistakes, however the Classic API allows one to do as one pleases.
+
+[^4]: Currently only available on nightly Rust, with the `nightly` feature flag enabled.
 
 */
 
