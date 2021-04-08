030fcf27d958401bc5cae5173310593b4894ec46
Brenden Matthews <brenden@brndn.io>
2021-04-08 16:54:51 -0500

    Update fuzz test.

--- fuzz/fuzz_targets/fuzz_target_1.rs
+++ fuzz/fuzz_targets/fuzz_target_1.rs
@@ -1,8 +1,8 @@
 #![no_main]
 use libfuzzer_sys::fuzz_target;
 
-use dryoc::generichash::GenericHash;
+use dryoc::generichash::{GenericHash, Key};
 
 fuzz_target!(|data: &[u8]| {
-    GenericHash::hash_with_defaults_to_vec(data, None);
+    GenericHash::hash_with_defaults_to_vec::<_, Key>(data, None).ok();
 });
